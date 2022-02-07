f9ce301e328f02013a7aa45f5718460f4849fdaf
Benjamin Zhang <bzh@ocf.berkeley.edu>
2022-02-07 04:04:30 -0800

    Add constants file for paths.

---
+++ constants.py
@@ -0,0 +1,3 @@
+CLAMATO_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/clamato_dr2'
+GAL_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/coeval'
+XCORR_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/xcorr'
