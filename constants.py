4191fb6605da00520bd4054874cd7c829982c7d0
Benjamin Zhang <bzh@ocf.berkeley.edu>
2022-04-29 00:48:00 -0700

    Add MCMC output plotting notebook.

--- constants.py
+++ constants.py
@@ -4,7 +4,9 @@ CLAMATO_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/clamato_dr2'
 GAL_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/coeval'
 XCORR_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/xcorr'
 MODEL_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/model'
+MCMC_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/data/mcmc'
 SIM_DIR_BASE = '/global/homes/b/bzh/projectdir/bzh/BolshoiP/full-halo-cat'
+FIG_DIR_BASE = '/global/homes/b/bzh/clamato-xcorr/figures'
 
 DATA_VERSION = 'v0'
 
