2763985587aed9e79c0a6ff8e46acffd342850ed
Benjamin Zhang <bzh@ocf.berkeley.edu>
2022-06-03 02:43:58 -0700

    Bugfixes; update MCMC slurm script.

--- mcmc_run.sh
+++ mcmc_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -C haswell
 #SBATCH -q flex
-#SBATCH --nodes=5
-#SBATCH -t 18:00:00
-#SBATCH --time-min 00:10:00
-#SBATCH -o mocksurvey_mcmc.out
+#SBATCH --nodes=4
+#SBATCH -t 8:00:00
+#SBATCH --time-min 00:15:00
+#SBATCH -o mcmc_run.out
 #SBATCH --open-mode=append
 #SBATCH --mail-user=benjamin.zhang@berkeley.edu
 #SBATCH --mail-type=END,FAIL
@@ -14,8 +14,12 @@
 #SBATCH --requeue
 #SBATCH --open-mode=append
 
+kill_parent() {
+	pkill -u bzh -o python3 --signal SIGINT
+}
+
 # specify the command to run to checkpoint your job if any (leave blank if none)
-ckpt_command=
+ckpt_command=kill_parent
 
 # requeueing the job if reamining time >0 (do not change the following 3 lines )
 . /usr/common/software/variable-time-job/setup.sh
@@ -25,11 +29,11 @@ requeue_job func_trap USR1
 module unload python
 module load python
 source ~/.bashrc
-conda activate clamato-xcorr
+conda activate vega
 cd /global/u1/b/bzh/clamato-xcorr
-srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MockSurvey_MCMC.py mcmc_cfg/3DHST.yaml &
-srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MockSurvey_MCMC.py mcmc_cfg/CLAMATO.yaml &
-srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MockSurvey_MCMC.py mcmc_cfg/MOSDEF.yaml &
-srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MockSurvey_MCMC.py mcmc_cfg/VUDS.yaml &
-srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MockSurvey_MCMC.py mcmc_cfg/zDeep.yaml &
+srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MCMC_Run.py mcmc_cfg/3DHST.yaml &
+srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MCMC_Run.py mcmc_cfg/CLAMATO.yaml &
+#srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MCMC_Run.py mcmc_cfg/MOSDEF.yaml &
+srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MCMC_Run.py mcmc_cfg/VUDS.yaml &
+srun -N 1 -n 1 -c 64 --cpu_bind=cores python3 MCMC_Run.py mcmc_cfg/zDeep.yaml &
 wait
