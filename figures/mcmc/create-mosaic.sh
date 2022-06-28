faf8ced6934a5c95ddce7155ee034d34c1777e6a
Benjamin Zhang <bzh@ocf.berkeley.edu>
2022-06-28 02:45:01 -0700

    Generate separate titled versions of corner plots; attach colorbars to mosaics; flip delta_z in MCMC corner plots to be consistent.

--- figures/mcmc/create-mosaic.sh
+++ figures/mcmc/create-mosaic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 surveys=(3dhst clamato mosdef vuds zDeep)
-montage -mode concatenate -tile 3x0 -background white -geometry -15-0 corner_3dhst.png corner_clamato.png corner_mosdef.png corner_vuds.png corner_zDeep.png corner_mosaic.png
-montage -mode concatenate -tile 3x0 -background white bestmodel_3dhst.png bestmodel_clamato.png bestmodel_mosdef.png bestmodel_vuds.png bestmodel_zDeep.png bestmodel_mosaic.png
+montage -mode concatenate -tile 3x0 -background white -geometry -15-0 titledcorner_3dhst.png titledcorner_clamato.png titledcorner_mosdef.png titledcorner_vuds.png titledcorner_zDeep.png corner_mosaic.png
+montage -mode concatenate -tile 3x0 -background white titledbestmodel_3dhst.png titledbestmodel_clamato.png titledbestmodel_mosdef.png titledbestmodel_vuds.png titledbestmodel_zDeep.png bestmodel_colorbar.png bestmodel_mosaic.png
 
 mkdir -p combined
 for survey in "${surveys[@]}" ; do
-    montage -mode concatenate -gravity Center -tile 2x1 -background white corner_$survey.png bestmodel_$survey.png combined/combined_$survey.png
+    montage -mode concatenate -gravity Center -tile 0x1 -background white titledcorner_$survey.png bestmodel_$survey.png bestmodel_colorbar.png combined/combined_$survey.png
 done
