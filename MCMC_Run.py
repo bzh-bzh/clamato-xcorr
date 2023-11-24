46224a118dd583c336fb5ad24f333797381ea91c
Benjamin Zhang <bzh@ocf.berkeley.edu>
2023-11-24 01:20:46 -0800

    Correctly normalize likelihood/priors in 1D MCMC case.

--- MCMC_Run.py
+++ MCMC_Run.py
@@ -25,6 +25,7 @@ import emcee
 import yaml
 import numpy as np
 from scipy.stats import multivariate_normal
+import scipy.linalg
 from matplotlib import pyplot as plt
 
 import constants
@@ -90,77 +91,9 @@ class EarlyTermEnsembleSampler(emcee.EnsembleSampler):
         # Store so that the ``initial_state=None`` case will work
         self._previous_state = results
         return results
-
-# Constants
-BASE_DIR = Path('/global/homes/b/bzh/clamato-xcorr/data/mcmc')
-
-# Order that these are initialized in is the order of the data vector theta which emcee uses.
-PARAM_LIMITS_ALL = {
-    'bias_QSO': (0, 10),
-    'beta_QSO': (0, 10),
-    'sigma_velo_disp_gauss_QSO': (0, 20),
-    'drp_QSO': (-10, 10),
-    'bias_hcd': (-1, 1),
-    'beta_hcd': (0, 50),
-    'L0_hcd': (0, 20)
-}
-
-assert len(sys.argv) == 2
-# Open config file and parse parameters
-with open(sys.argv[1], 'r') as f:
-    input_cfg = yaml.safe_load(f)
-# Special handling for beta_QSO.
-#if input_cfg['initial']['beta_QSO'] is None:
-#    input_cfg['initial']['beta_QSO'] = input_cfg['initial']['bias_QSO']**-1
-
-survey_name = input_cfg['survey']
-# Initialize best-guess vector and parameter limits.
-init_theta = []
-param_limits = collections.OrderedDict()
-for k, p in input_cfg['initial'].items():
-    if k == 'beta_QSO' and p is None:
-        print('beta_QSO initial value is none; fixing inverse relationship with bias_QSO.')
-        continue
-    init_theta.append(p)
-    param_limits[k] = PARAM_LIMITS_ALL[k]
-init_theta = np.array(init_theta)
-assert np.issubdtype(init_theta.dtype, np.number)
-assert len(param_limits) == len(init_theta)
-# Parameter limit overrides.
-if 'limit_overrides' in input_cfg and input_cfg['limit_overrides']:
-    for param_name, lim in input_cfg['limit_overrides'].items():
-        print(f'Overriding limit for {param_name} to be {lim}.')
-        param_limits[param_name] = tuple(lim)
-n_dim = len(init_theta)
-n_walkers = input_cfg['n_walkers']
-n_step = input_cfg['n_step']
-seed = input_cfg['rng_seed']
-# If resuming from a backend hdf5 file, the last step's random state is re-used, so we don't need to worry about
-# stale RNG for repeated short-length runs of this script.
-np.random.seed(seed)
-n_proc = input_cfg['n_processes']
-if n_proc == -1:
-    n_proc = multiprocessing.cpu_count()
-if 'base_dir' in input_cfg:
-    BASE_DIR = Path(input_cfg['base_dir'])
-
-data_dir = BASE_DIR / survey_name
-vega = VegaInterface(data_dir / f'main_{survey_name}.ini')
-assert not vega.priors
-print(np.sum(vega.data['qsoxlya'].mask))
-
-# Fixed parameters in config.
-if 'fixed' in input_cfg and input_cfg['fixed']:
-    for k, p in input_cfg['fixed'].items():
-        assert k not in param_limits
-        vega.params[k] = p
-# Priors in config.
-if 'priors' in input_cfg.keys() and input_cfg['priors']:
-    vega.priors = input_cfg['priors']
     
-# See if we're working in one dimension. If so, monkey-patch the Vega chi2 function.
-def one_dimensional_chi2(self, params=None, direct_pk=None):
-    """Compute full chi2 for all components.
+def one_dimensional_log_lik(self, params=None, direct_pk=None, return_aux=False):
+    """Compute full log likelihood for all components.
 
     Parameters
     ----------
@@ -172,7 +105,7 @@ def one_dimensional_chi2(self, params=None, direct_pk=None):
     Returns
     -------
     float
-        chi^2
+        log Likelihood
     """
     assert self._has_data
 
@@ -197,6 +130,7 @@ def one_dimensional_chi2(self, params=None, direct_pk=None):
 
     # Go trough each component and compute the chi^2
     chi2 = 0
+    log_norm = 0
     for name in self.corr_items:
         try:
             if direct_pk is None:
@@ -254,71 +188,162 @@ def one_dimensional_chi2(self, params=None, direct_pk=None):
         reduced_cov = reduced_cov.filled(fill_value=0)
         reduced_invcov = np.linalg.inv(reduced_cov)
         
+        # Compute the normalizations here, since we've changed the dimension of the data.
+        log_norm -= 0.5 * data_masked.size * np.log(2 * np.pi)
+        if self.monte_carlo:
+            raise NotImplementedError
+        else:
+            _, d, _ = scipy.linalg.ldl(reduced_cov)
+            log_cov_det = np.log(d.diagonal()).sum()
+            log_norm -= 0.5 * log_cov_det
+        
         if self.monte_carlo:
             raise NotImplementedError
         else:
             diff = reduced_data - reduced_model
             chi2 += diff.T.dot(reduced_invcov.dot(diff))
+            
+    raw_chi2 = chi2
 
     # Add priors
     for param, prior in self.priors.items():
         chi2 += self._gaussian_chi2_prior(local_params[param], prior[0], prior[1])
+        
+    # Compute log lik
+    log_lik = log_norm - 0.5 * chi2
 
-    assert isinstance(chi2, float)
-    return chi2
+    # Add priors normalization
+    for param, prior in self.priors.items():
+        log_lik += self._gaussian_lik_prior(prior[1])
 
-if input_cfg['one_dimension']:
-    VegaInterface.chi2 = one_dimensional_chi2
-
-# Defined after we load the vega interface so we don't have massive pickling overhead when multiprocessing.
-def log_likelihood(theta):
-    # Since the vega object is copied into each process, we should be able to get away with mutating the params dict stored in it.
-    # Not so if we decide to do threading, for whatever reason!
-    for p, k in zip(theta, param_limits.keys()):
-        vega.params[k] = p
-    if not 'beta_QSO' in param_limits.keys():
-        vega.params['beta_QSO'] = vega.params['growth_rate'] / vega.params['bias_QSO']
-    # This correctly includes normalization between priors and likelihoods.
-    return vega.log_lik()
-
-def check_bounds(theta):
-    for p, bound in zip(theta, param_limits.values()):
-        if not bound[0] <= p <= bound[1]:
-            return False
-    return True
-
-# Flat
-def log_prior(theta):
-    if check_bounds(theta):
-        # Extrapolating; don't let the walkers go outside this area by returning -inf.
-        return 0
+    assert isinstance(log_lik, float)
+    if return_aux:
+        return log_lik, (raw_chi2, reduced_cov, mask_nonflat)
     else:
-        return -np.inf
+        return log_lik
 
-def log_prob(theta):
-    lp = log_prior(theta)
-    if not np.isfinite(lp):
-        return -np.inf
+
+# Constants
+BASE_DIR = Path('/global/homes/b/bzh/clamato-xcorr/data/mcmc')
+
+# Order that these are initialized in is the order of the data vector theta which emcee uses.
+PARAM_LIMITS_ALL = {
+    'bias_QSO': (0, 10),
+    'beta_QSO': (0, 10),
+    'sigma_velo_disp_gauss_QSO': (0, 20),
+    'drp_QSO': (-10, 10),
+    'bias_hcd': (-1, 1),
+    'beta_hcd': (0, 50),
+    'L0_hcd': (0, 20)
+}
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    # Open config file and parse parameters
+    with open(sys.argv[1], 'r') as f:
+        input_cfg = yaml.safe_load(f)
+    # Special handling for beta_QSO.
+    #if input_cfg['initial']['beta_QSO'] is None:
+    #    input_cfg['initial']['beta_QSO'] = input_cfg['initial']['bias_QSO']**-1
+
+    survey_name = input_cfg['survey']
+    # Initialize best-guess vector and parameter limits.
+    init_theta = []
+    param_limits = collections.OrderedDict()
+    for k, p in input_cfg['initial'].items():
+        if k == 'beta_QSO' and p is None:
+            print('beta_QSO initial value is none; fixing inverse relationship with bias_QSO.')
+            continue
+        init_theta.append(p)
+        param_limits[k] = PARAM_LIMITS_ALL[k]
+    init_theta = np.array(init_theta)
+    assert np.issubdtype(init_theta.dtype, np.number)
+    assert len(param_limits) == len(init_theta)
+    # Parameter limit overrides.
+    if 'limit_overrides' in input_cfg and input_cfg['limit_overrides']:
+        for param_name, lim in input_cfg['limit_overrides'].items():
+            print(f'Overriding limit for {param_name} to be {lim}.')
+            param_limits[param_name] = tuple(lim)
+    n_dim = len(init_theta)
+    n_walkers = input_cfg['n_walkers']
+    n_step = input_cfg['n_step']
+    seed = input_cfg['rng_seed']
+    # If resuming from a backend hdf5 file, the last step's random state is re-used, so we don't need to worry about
+    # stale RNG for repeated short-length runs of this script.
+    np.random.seed(seed)
+    n_proc = input_cfg['n_processes']
+    if n_proc == -1:
+        n_proc = multiprocessing.cpu_count()
+    if 'base_dir' in input_cfg:
+        BASE_DIR = Path(input_cfg['base_dir'])
+
+    data_dir = BASE_DIR / survey_name
+    vega = VegaInterface(data_dir / f'main_{survey_name}.ini')
+    assert not vega.priors
+    print(np.sum(vega.data['qsoxlya'].mask))
+
+    # Fixed parameters in config.
+    if 'fixed' in input_cfg and input_cfg['fixed']:
+        for k, p in input_cfg['fixed'].items():
+            assert k not in param_limits
+            vega.params[k] = p
+    # Priors in config.
+    if 'priors' in input_cfg.keys() and input_cfg['priors']:
+        vega.priors = input_cfg['priors']
+
+    # See if we're working in one dimension. If so, monkey-patch the Vega log-likelihood function.
+    if input_cfg['one_dimension']:
+        VegaInterface.log_lik = one_dimensional_log_lik
+
+    # Defined after we load the vega interface so we don't have massive pickling overhead when multiprocessing.
+    def log_likelihood(theta):
+        # Since the vega object is copied into each process, we should be able to get away with mutating the params dict stored in it.
+        # Not so if we decide to do threading, for whatever reason!
+        for p, k in zip(theta, param_limits.keys()):
+            vega.params[k] = p
+        if not 'beta_QSO' in param_limits.keys():
+            vega.params['beta_QSO'] = vega.params['growth_rate'] / vega.params['bias_QSO']
+        # This correctly includes normalization between priors and likelihoods.
+        return vega.log_lik()
+
+    def check_bounds(theta):
+        for p, bound in zip(theta, param_limits.values()):
+            if not bound[0] <= p <= bound[1]:
+                return False
+        return True
+
+    # Flat
+    def log_prior(theta):
+        if check_bounds(theta):
+            # Extrapolating; don't let the walkers go outside this area by returning -inf.
+            return 0
+        else:
+            return -np.inf
+
+    def log_prob(theta):
+        lp = log_prior(theta)
+        if not np.isfinite(lp):
+            return -np.inf
+        else:
+            return log_likelihood(theta) + lp
+
+    # print(f'Bounds of bias are [{model_func.bias_lim[0]}, {model_func.bias_lim[1]}]')
+    # print(f'Bounds of sigz are [{model_func.sigz_lim[0]}, {model_func.sigz_lim[1]}]')
+
+    walker_pos = np.random.normal(loc=init_theta, scale=1e-4, size=(n_walkers, n_dim))
+    for i in range(len(walker_pos)):
+        while not check_bounds(walker_pos[i]):
+            walker_pos[i] = np.random.normal(loc=init_theta, scale=1e-4, size=(n_dim,))
+
+    chain_file_path = data_dir / f'chain_{survey_name}{"_" + input_cfg["chain_file_suffix"] if input_cfg["chain_file_suffix"] else ""}.hdf5'
+    backend = SafeHDFBackend(chain_file_path)
+    if not os.path.exists(chain_file_path) or (not backend.iteration):
+        print(f'Backend file {chain_file_path} is empty: initializing a new chain.')
+        backend.reset(n_walkers, n_dim)
     else:
-        return log_likelihood(theta) + lp
+        print(f'Resuming from backend file {chain_file_path}, with length {backend.iteration}.')
+        walker_pos = None
 
-# print(f'Bounds of bias are [{model_func.bias_lim[0]}, {model_func.bias_lim[1]}]')
-# print(f'Bounds of sigz are [{model_func.sigz_lim[0]}, {model_func.sigz_lim[1]}]')
-
-walker_pos = np.random.normal(loc=init_theta, scale=1e-4, size=(n_walkers, n_dim))
-for i in range(len(walker_pos)):
-    while not check_bounds(walker_pos[i]):
-        walker_pos[i] = np.random.normal(loc=init_theta, scale=1e-4, size=(n_dim,))
-
-chain_file_path = data_dir / f'chain_{survey_name}{"_" + input_cfg["chain_file_suffix"] if input_cfg["chain_file_suffix"] else ""}.hdf5'
-backend = SafeHDFBackend(chain_file_path)
-if not os.path.exists(chain_file_path) or (not backend.iteration):
-    print(f'Backend file {chain_file_path} is empty: initializing a new chain.')
-    backend.reset(n_walkers, n_dim)
-else:
-    print(f'Resuming from backend file {chain_file_path}, with length {backend.iteration}.')
-    walker_pos = None
-
-with multiprocessing.Pool(n_proc) as p:
-    sampler = EarlyTermEnsembleSampler(n_walkers, n_dim, log_prob, pool=p, backend=backend)
-    sampler.run_mcmc(walker_pos, n_step - backend.iteration, progress=True)
+    with multiprocessing.Pool(n_proc) as p:
+        sampler = EarlyTermEnsembleSampler(n_walkers, n_dim, log_prob, pool=p, backend=backend)
+        sampler.run_mcmc(walker_pos, n_step - backend.iteration, progress=True)
